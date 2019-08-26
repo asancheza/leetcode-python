4b05a2256fdf977be6f203d35b97fe0372575ef0
Alejandro Sanchez Acosta <sancheza@adobe.com>
2019-08-26 11:24:22 -0700

    Updated

--- 017-letter-combinations-phone-number-dialpad.py
+++ 017-letter-combinations-phone-number-dialpad.py
@@ -1,15 +1,6 @@
 class Solution:
     # @return a list of strings, [s1, s2]
-    def letterCombinations(self, digits):
-        def dfs(num, string, res):
-            if num == length:
-                res.append(string)
-                return
-
-            for letter in dict[digits[num]]:
-                print letter
-                dfs(num+1, string+letter, res)
-        
+    def dfs(self, num, string, res, length, digits):
         dict = {'1': [' '],
                 '2':['a','b','c'],
                 '3':['d','e','f'],
@@ -19,10 +10,19 @@ class Solution:
                 '7':['p','q','r','s'],
                 '8':['t','u','v'],
                 '9':['w','x','y','z']
-                }
+        }
+
+        if num == length:
+            res.append(string)
+            return
+
+        for letter in dict[digits[num]]:
+            self.dfs(num+1, string+letter, res, length, digits)
+
+    def letterCombinations(self, digits):
         res = []
         length = len(digits)
-        dfs(0, '', res)
+        self.dfs(0, '', res, length, digits)
         return res
 
-print Solution().letterCombinations('23')
+print Solution().letterCombinations('239')
